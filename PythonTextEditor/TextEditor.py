3a286f33503040bfbc1d3798ba2a8817a2b42a73
Dylan Hourigan <K00260307@student.lit.ie>
2022-12-22 22:49:59 +0000

    added error catching, and new icon/title

--- PythonTextEditor/TextEditor.py
+++ PythonTextEditor/TextEditor.py
@@ -5,22 +5,37 @@ from tkinter import filedialog
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 root = Tk("Text Editor")
-root.title = dir_path
+root.title("Text Editor")
+root.iconbitmap("icon.ico")
 text = Text(root)
 text.grid()
 
 def saveAs():
     global text
     t = text.get("1.0", "end-1c")
-    saveLocation = filedialog.asksaveasfilename(initialdir= dir_path, title="Save Text File", filetypes= (("Text Files", "*.txt"),))
-    file1=open(saveLocation,'w')
-    file1.write(t)
-    file1.close()
+
+    try:
+        saveLocation = filedialog.asksaveasfilename(initialdir= dir_path, title="Save Text File", filetypes= (("Text Files", "*.txt"),))
+        file1 = open(saveLocation, 'w')
+        file1.write(t)
+    except Exception as e:
+        # handle the exception
+        print("An error occurred:", e)
+    finally:
+        # close the file regardless of whether an exception occurred
+        file1.close()
 
 def openFile():
-    openLocation = filedialog.askopenfilename(initialdir= dir_path, title="Open Text File", filetypes= (("Text Files", "*.txt"),))
-    file1=open(openLocation,'r')
-    text.insert("1.0", file1.read())
+    try:
+        openLocation = filedialog.askopenfilename(initialdir= dir_path, title="Open Text File", filetypes= (("Text Files", "*.txt"),))
+        file1 = open(openLocation, 'r')
+        text.insert("1.0", file1.read())
+    except Exception as e:
+        # handle the exception
+        print("An error occurred:", e)
+    finally:
+        # close the file regardless of whether an exception occurred
+        file1.close()
 
 
 button = Button(root, text="Save", command=saveAs)
