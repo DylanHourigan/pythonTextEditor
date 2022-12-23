0a9f8ad0d23dd64b865782ecdae79e1c9ae17e5e
Dylan Hourigan <K00260307@student.lit.ie>
2022-12-23 00:32:11 +0000

    removed defunct code and added comments

--- PythonTextEditor/TextEditor.py
+++ PythonTextEditor/TextEditor.py
@@ -3,15 +3,21 @@ import os
 from tkinter import *
 from tkinter import filedialog
 
+#Finds the directory path for files stored in the project. I used this for the inital directory when opening or saving text files.
 dir_path = os.path.dirname(os.path.realpath(__file__))
+
+#creates the window
 root = Tk("Text Editor")
 root.title("Text Editor")
 root.iconbitmap("icon.ico")
 root.configure(bg="#FFFFFF")
+
+#creates the text box
 text = Text(root)
 text.pack(side="top", fill="both", expand=True)
 text.configure(bg="#FFFFFF",fg="#000000")
 
+#Save Button function
 def saveAs():
     global text
     t = text.get("1.0", "end-1c")
@@ -27,6 +33,7 @@ def saveAs():
         # close the file regardless of whether an exception occurred
         file1.close()
 
+#open button function
 def openFile():
     try:
         openLocation = filedialog.askopenfilename(initialdir= dir_path, title="Open Text File", filetypes= (("Text Files", "*.txt"),))
@@ -38,16 +45,8 @@ def openFile():
     finally:
         # close the file regardless of whether an exception occurred
         file1.close()
-def backgroundWhite():
-    root.configure(bg="#FFFFFF")
-    text.configure(fg="#FFFFFF")
-def backgroundBlack():
-    root.configure(bg="#000000")
-    text.configure(fg="#FFFFFF")
-def backgroundBlue():
-    root.configure(bg="#001899")
-    text.configure(fg="#FFFFFF")
 
+#background color option menu function
 def changeColour(colour):
     if(colour == "White"):
         root.configure(bg="#FFFFFF")
@@ -64,9 +63,11 @@ def changeColour(colour):
     else:
         root.configure(bg="#FFFFFF",fg="#000000")
         
-
+#Save Button
 button = Button(root, text="Save", command=saveAs)
 button.pack(side="left")
+
+#Open Button
 button2 = Button(root, text="Open", command=openFile)
 button2.pack(side="left")
 
@@ -77,17 +78,9 @@ clicked.set("White")
 drop = OptionMenu(root, clicked, "White", "Black", "Blue", command= changeColour)
 drop.pack(side="right")
 
-# background = Menubutton(root, text="Background Colour")
-# menu=Menu(root, tearoff=0)
-# background.config(menu=menu)
-# whitebg = IntVar()
-# blackbg = IntVar()
-# bluebg = IntVar()
-# menu.add(label="White", command=backgroundWhite, itemType="checkbutton")
-# menu.add(label="Black", command=backgroundBlack, itemType="checkbutton")
-# menu.add(label="Blue", command=backgroundBlue, itemType="checkbutton")
-# background.pack(side="left")
-
+#Exit Button
 exitbutton = Button(root, text="Exit", command=root.destroy)
 exitbutton.pack(side="left")
+
+
 root.mainloop()
